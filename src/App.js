1d82f920691c40ce0d5fafdd3dea3da02e505f43
adrianglez2203 <adrianglez2203@gmail.com>
2022-04-09 19:09:20 -0400

    message2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { NextUIProvider } from '@nextui-org/react';
+import { Grid } from "@nextui-org/react";
+import { Card1 } from './Card1';
+import { Card2 } from './Card2';
+import { Card5 } from './Card3';
+import { Card4 } from './Card4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider>
+    <Grid.Container gap={2} justify="center">
+      <Grid xs={12} sm={4}>
+        <Card1 />
+      </Grid>
+      <Grid xs={12} sm={4}>
+        <Card2 />
+      </Grid>
+      <Grid xs={12} sm={5}>
+        <Card4 />
+      </Grid>
+      <Grid xs={12} sm={7}>
+        <Card5 />
+      </Grid>
+    </Grid.Container>
+    </NextUIProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { NextUIProvider } from '@nextui-org/react';
 ReactDOM.render(
   <React.StrictMode>
     <App />
